d566c886993a96075416d3696d956c7ee32b8290
Jimmy <dm.a.uvarov@gmail.com>
2022-02-24 16:16:01 -0500

    commit tree c1ce6fb1734ac59f84d86ee5e3116825368cee6e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    
+        @this_dogs_name 
+    
+        # the setter instance <-method name=() setting @this_dogs_name to argument  
+        def name=(dog_name)
+            @this_dogs_name = dog_name
+        end
+    
+        # getter
+        def name
+            @this_dogs_name
+        end
 
 end
